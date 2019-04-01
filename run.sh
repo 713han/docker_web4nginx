4adb19577d646ae642ec988c5f82759ac06e7740
HansHuang <713han@gmail.com>
2019-04-01 16:20:03 +0800

    fix shell script

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --name nginx_ubuntu -v ~/docker/www/:/home/www/ -v ~/docker/hosts/:/etc/nginx/sites-enabled/ -p 80:80 -d 713han/ubuntu-nginx
+docker run --name nginx_ubuntu -v ~/docker/www/:/home/www/ -v ~/docker/hosts/:/etc/nginx/sites-enabled/ -p 80:80 -d 713han/ubuntu-nginx-php
