f4bc6f59f313741c4e31d48d406f5bd43e444a80
HansHuang <713han@gmail.com>
2019-04-02 15:47:35 +0800

    fix docker env

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --name php_ubuntu -v ~/docker/www/:/home/www/ -v ~/docker/hosts/:/etc/nginx/sites-enabled/ -p 80:80 -d 713han/ubuntu_nginx_php7
+docker run --name php_ubuntu --link mysql_5.7 -v ~/docker/www/:/home/www/ -v ~/docker/hosts/:/etc/nginx/sites-enabled/ -p 80:80 -d 713han/ubuntu_nginx_php7
