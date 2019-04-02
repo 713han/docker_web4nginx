11a17374405f1ef4a3914ee1c877e59612a2a758
HansHuang <713han@gmail.com>
2019-04-02 17:34:48 +0800

    fix script

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
+docker stop php_ubuntu
+docker rm php_ubuntu
 docker run --name php_ubuntu --link mysql_5.7 -v ~/docker/home/www-data:/home/www-data -v ~/docker/www/:/home/www/ -v ~/docker/hosts/:/etc/nginx/sites-enabled/ -p 80:80 -d 713han/ubuntu_nginx_php7
+docker ps
--- www/default/batch/clean_ah_api.sh
+++ www/default/batch/clean_ah_api.sh
@@ -1,14 +1,13 @@
-rm -rf /home/www/ah-api/webroot
-git clone git@tmnewa.github.com:hans4tmnewa/ah-api-php.git /home/www/ah-api/webroot
+#rm -rf /home/www/ah-api/webroot
+#git clone git@tmnewa.github.com:hans4tmnewa/ah-api-php.git /home/www/ah-api/webroot
 cd /home/www/ah-api/webroot
 
-if [ -n "$2" ]; then
-        git checkout $2
-fi
+# if [ -n "$2" ]; then
+#         git checkout $2
+# fi
 
 # composer update
 composer install
-
 case "$1" in
         prod)
                 php init --env=Production --overwrite=All
