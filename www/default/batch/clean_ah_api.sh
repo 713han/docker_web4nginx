3510132a29b3469f91a94f86270e5e4bf890fc4e
HansHuang <713han@gmail.com>
2019-04-02 15:48:03 +0800

    fix docker env

---
+++ www/default/batch/clean_ah_api.sh
@@ -0,0 +1,29 @@
+rm -rf /home/www/ah-api/webroot
+git clone git@github.com:hans4tmnewa/ah-api-php.git /home/www/ah-api/webroot
+cd /home/www/ah-api/webroot
+
+if [ -n "$2" ]; then
+        git checkout $2
+fi
+
+# composer update
+composer install
+
+case "$1" in
+        prod)
+                php init --env=Production --overwrite=All
+                echo $1
+                ;;
+        test)
+                php init --env=Test --overwrite=All
+                echo $1
+                ;;
+        dev)
+                php init --env=Development --overwrite=All
+                echo $1
+                ;;
+        *)
+                echo "./clean_ah_api [dev/test/prod] [branch name]"
+                ;;
+esac
+
